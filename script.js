25035e80a2eff16403cd569670b8929320f8bb01
zau19 <54337274+zau19@users.noreply.github.com>
2019-08-28 19:03:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// var count = -10;
+
+// while (count < 19){
+//     console.log(count);
+//     count++;
+// }
+
+// var count = 10;
+
+// while (count <= 40) {
+//     if (count % 2 === 0) {
+//         console.log(count);
+//     }
+//     count++;
+// }
+
+// var count = 300;
+
+// while (count <= 333) {
+//     if (count % 2 !== 0) {
+//         console.log(count);
+//     }
+//     count++;
+// }
+
+// var count = 5;
+
+// while (count <= 50 ) {
+//     if (count % 5 == 0 ){
+//         console.log(count);
+//     }
+//     count++;
+// }
+
+// var count = 5;
+
+// while (count <= 50 ) {
+//     if (count % 3 == 0 ){
+//         console.log(count);
+//     }
+//     count++;
+// }
